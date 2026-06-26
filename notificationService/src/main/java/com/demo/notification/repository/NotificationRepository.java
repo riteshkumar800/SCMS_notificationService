f86e4af03c507b22bf9b2347721429cc414f9b76
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-06-27 02:42:07 +0530

    fixed

--- notificationService/src/main/java/com/demo/notification/repository/NotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.notification.repository;
-
-import com.demo.notification.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-
-}
